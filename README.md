7fccb7e410fd788bc7c0f433f8b9e2e5125eb58a
Jako <43934830+jfrb95@users.noreply.github.com>
2024-09-17 06:54:19 +0100

    uploading completed project

---
+++ README.md
@@ -0,0 +1,1 @@
+A library app to test various skills. Not currently uploaded to github. not made to look nice yet
---
+++ script.js
@@ -0,0 +1,101 @@
+(function() {
+
+    const log = console.log;
+
+    const testButton = document.querySelector(".test");
+
+    const libraryTable = document.querySelector("table");
+    const newBookButton = document.querySelector(".new-book-button");
+    const dialog = document.querySelector("dialog");
+    const dialogCancelButton = document.querySelector(".cancel");
+    const dialogAddBookButton = document.querySelector(".add-book");
+    const form = document.querySelector(".new-book-form");
+
+    newBookButton.addEventListener("click", () => {
+        dialog.showModal();
+    });
+    dialogCancelButton.addEventListener("click", () => {
+        dialog.close();
+    });
+    dialogAddBookButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const fd = new FormData(form);
+        addBookToLibrary(
+            fd.get("title"),
+            fd.get("author"),
+            fd.get("num-pages"),
+            fd.get("have-read") === "yes" ? true : false
+        );
+        dialog.close();
+    });
+
+    testButton.addEventListener("click", () => {
+
+    });
+
+    const myLibrary = [new Book("title 1", "author 1", 5, true), new Book("title 2", "author 2", 821, false)];
+
+    function Book(title, author, numPages, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.haveRead = haveRead;
+
+        this.info = function() {
+            return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.haveRead ? "finished reading" : "not read yet"}`
+        }
+
+        this.changeRead = function() {
+            this.haveRead ? this.haveRead = false : this.haveRead = true;
+        }
+    }
+
+    function addBookToLibrary(title, author, numPages, haveRead) {
+        const book = new Book(...arguments);
+        book.libraryIndex = myLibrary.length;
+        myLibrary.push(book);
+        displayLibrary();
+    }
+
+    function displayLibrary() {
+        const newTableBody = document.createElement("tbody");
+        myLibrary.forEach((book, index) => {
+            const row = newTableBody.insertRow(-1);
+
+            row.dataset.libraryIndex = index;
+
+            const title = row.insertCell()
+            title.innerText = book.title;
+
+            const author = row.insertCell()
+            author.innerText = book.author;
+
+            const numPages = row.insertCell()
+            numPages.innerText = book.numPages;
+
+            const haveRead = row.insertCell()
+            haveRead.innerText = book.haveRead ? "finished reading" : "not yet read";
+            const toggleRead = document.createElement("button");
+            toggleRead.textContent = "Toggle Read";
+            toggleRead.addEventListener("click", () => {
+                const book = myLibrary[row.dataset.libraryIndex];
+                log(book);
+                book.haveRead ? book.haveRead = false : book.haveRead = true;
+                displayLibrary();
+            })
+            haveRead.appendChild(toggleRead);
+
+            const newCell = row.insertCell();
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Remove Book";
+            deleteButton.addEventListener("click", () => {
+                myLibrary.splice(row.dataset.libraryIndex, 1);
+                displayLibrary();
+            })
+            newCell.appendChild(deleteButton);
+
+            newTableBody.appendChild(row);
+        });
+        libraryTable.replaceChild(newTableBody, document.querySelector("tbody"));
+    }
+})();
